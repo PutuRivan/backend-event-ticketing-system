71340a70da557f74b63af71ca38b771317d9fc03
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2026-08-05 14:39:35 +0700

    feat: enhance CORS configuration in main application setup, allowing specific origins, methods, and headers for improved security and flexibility

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,21 @@ async function bootstrap() {
   });
 
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: config.app.url,
+    credentials: true,
+    methods: [
+      'GET',
+      'POST',
+      'PATCH',
+      'DELETE',
+      'OPTIONS',
+    ],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+    ],
+  });
 
   // Set global prefix
   app.setGlobalPrefix('api');
